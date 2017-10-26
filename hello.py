aafadfe49d5c7cd685ad169114ec359dcc03fb0f
Sergey Shimanskiy <flyer8@yandex.ru>
2017-10-26 22:35:52 +0300

    fixed

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Autor: Sergey Shimanskiy
-# This is test 888
+# This is test revision 11
 print("Hello World!")
-print("Hi Paragon Team, This is test task 888")
+print("Hi Paragon Team, This is test revision 11")
+print("Hi guys, I found out, that the revision just has not been accepted.))")
